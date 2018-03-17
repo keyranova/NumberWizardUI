0fe489d55d5028367e9594c302a75464c6532e42
kurtnovack <kurt@walltowall.com>
2018-03-16 22:57:58 -0400

    imports number wizard script and completes UI

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+
+}
